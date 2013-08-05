14a1d700005c955a9245ef666e008bd0cfa03ed8
axel.grude@gmail.com <axel.grude@gmail.com@66088986-57c7-e302-0ba0-784ad26a101e>
2013-08-05 11:15:04 +0000

    titlebar cleaner 0.4.1

---
+++ trunk/defaults/preferences/prefs.js
@@ -0,0 +1,10 @@
+// Default preference values. These are accessible via the preferences system
+// or via the optional chrome/content/options.xul preferences dialog.
+// These are here as an example only. Please remove them or rename them to
+// something useful.
+
+// pref("extensions.titlebar_cleaner.boolpref", false);
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+
+pref("extensions.titlebarCleaner@agrude.com.description", "chrome://titlebarcleaner/locale/overlay.properties");
+pref("extensions.titlebarCleaner.firstRun", true);
